332eac2ed6c6baff3f77895d9e9f48dda9541602
Fedorovvn <89463822+Fedorovvn@users.noreply.github.com>
2021-10-23 00:43:13 +0300

    Выполненное задание на проверку

---
+++ src/main/java/web/servise/UserService.java
@@ -0,0 +1,14 @@
+package web.servise;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    User getUser(Long id);
+    void deleteUser(Long id);
+    List<User> listUsers();
+    void changeNickname(Long id, String newNickname);
+    void changeEmail(Long id, String newEmail);
+}
---
+++ src/main/java/web/servise/UserServiceImp.java
@@ -0,0 +1,49 @@
+package web.servise;
+
+import web.dao.UserDao;
+import web.model.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    private final UserDao userDao;
+
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional
+    @Override
+    public void add(User user) { userDao.add(user); }
+
+    @Transactional
+    @Override
+    public User getUser(Long id) {
+        return userDao.getUser(id);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional
+    @Override
+    public void changeNickname(Long id, String newNickname){ userDao.changeNickname(id, newNickname);}
+
+    @Transactional
+    @Override
+    public void changeEmail(Long id, String newEmail){ userDao.changeEmail(id, newEmail); }
+
+}
